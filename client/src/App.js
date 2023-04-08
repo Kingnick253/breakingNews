5c45ef89b1211e0917bdd5bf5e5e8b31236da5a4
Kingnick253 <nicholaswebb2016@gmail.com>
2023-04-08 00:54:34 -0700

    got express working server is successfully starting on the port 5000

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,9 @@
+import React from 'react'
 
-import './App.css';
-
-export default function App() {
+const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>App</div>
   )
 }
+
+export default App
